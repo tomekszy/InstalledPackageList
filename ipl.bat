af3296182c063b1c551483906b9db8fd5c873123
tszymanski <tszymanski@PC426.mir.local>
2018-03-01 13:39:20 +0100

    zmiana sposobu uruchamiania

--- ipl.bat
+++ ipl.bat
@@ -1,5 +1,7 @@
 @echo off
-.\adb\adb shell pm list packages -f > ./in.txt
+.\adb\adb.exe shell pm list packages -f > ./in.txt
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_162
-.\maven\bin\mvn exec:java -Dexec.mainClass=com.mycompany.installedpackagelist.Main
+call .\maven\bin\mvn.bat clean > nul
+call .\maven\bin\mvn.bat install > nul
+call .\maven\bin\mvn.bat exec:java -Dexec.mainClass=com.mycompany.installedpackagelist.Main
 pause
